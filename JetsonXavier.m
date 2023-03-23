704ed66edee179153dce0558b182f8300eb4886f
yongkgu <sejong04250@naver.com>
2023-03-23 17:21:20 +0900

    1.1

--- JetsonXavier.m
+++ JetsonXavier.m
@@ -28,16 +28,43 @@ disp('Target Check')
 
 % inputArgs = {ones(608,608,3,'uint8'),coder.Constant(matFile)};
 inputArgs = {coder.Constant(matFile)};
-
+% codegen -config cfg Detect_test -args inputArgs -report
 codegen -config cfg yolov3Detect -args inputArgs -report
-% hwobj.putFile('gpio_test.py', hwobj.workspaceDir);
+
 
 %% run
 
-% exe = [obj.workspaceDir '/yolov3Detect.elf'];
-exe = ['/home/wecar/remoteBuildDir/MATLAB_ws/R2022b/C/Users/Admin/Desktop/YONG/LiDAR/RPLidar/Jetson_AGX_Xavier/yolov3Detect.elf'];
+exe = [obj.workspaceDir '/yolov3Detect.elf'];
+% exe = ['/home/wecar/remoteBuildDir/MATLAB_ws/R2023a/C/Users/Admin/Desktop/YONG/LiDAR/RPLidar/Jetson_AGX_Xavier/yolov3Detect.elf'];
 pid = obj.runExecutable(exe);
-%% 
+%% run Detect_test
+
+% exe = [obj.workspaceDir '/Detect_test.elf'];
+exe = ['/home/wecar/remoteBuildDir/MATLAB_ws/R2023a/C/Users/Admin/Desktop/YONG/LiDAR/RPLidar/Jetson_AGX_Xavier/Detect_test.elf'];
+pid = obj.runExecutable(exe);
+%% stop
+
+killApplication(obj,exe)  
+killProcess(obj,pid)
+
+%% modbus read
+m = modbus('tcpip','192.168.0.81',1478)
+%% sample rate = 90 Hz
+ 
+tic;
+data = zeros(3000,4);
+count=1;
+while toc < 10
+data(count,1) = read(m, 'holdingregs',31);
+data(count,2) =read(m, 'holdingregs',32);
+data(count,3) =read(m, 'holdingregs',33);
+data(count,4) =read(m, 'holdingregs',34);
+% disp(data)
+count=count+1;
+
+end
+
+%% Generate File and Read File
 tic;
 getFile(obj,'/home/wecar/remoteBuildDir/MATLAB_ws/R2022b/C/Users/Admin/Desktop/YONG/LiDAR/RPLidar/Jetson_AGX_Xavier/idx.bin')
 fid = fopen('idx.bin', 'rb');
@@ -45,21 +72,13 @@ data = fread(fid,'uint8');
 disp(data)
 fclose(fid);
 toc
+
+
+%% Modbus test
+% codegen -config cfg Modbus_Client
+codegen -config cfg Modbus
+%% Modbus run
+exe = ['/home/wecar/remoteBuildDir/MATLAB_ws/R2023a/C/Users/Admin/Desktop/YONG/LiDAR/RPLidar/Jetson_AGX_Xavier/Modbus.elf'];
+runApplication(obj,'Modbus');
 %% stop
-
-killApplication(obj,exe)
-killProcess(obj,pid)
-%% 
-
-cam = webcam(obj,1,'640x480')
-
-%% 
-% detector = yolov3ObjectDetector('darknet53-coco');
-% matFile = 'pretrainedYOLOv3Detector.mat';
-% save(matFile,'detector');
-%% 
-if size(bboxes) == [0 4]
-    disp(1)
-else
-    disp(2)
-end
+killApplication(obj,'Modbus');
